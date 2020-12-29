9e09d7a738b30c45332806f558be3cb232e7c91f
sake <sakelog.website@gmail.com>
2020-12-29 14:09:35 +0900

    career style作成

--- src/component/profile/_career.tsx
+++ src/component/profile/_career.tsx
@@ -5,12 +5,13 @@ const Carrer: profile.component.func = (props) => {
   const jsonContent: profile.career_jsonContent = props.json.content;
 
   const career_list = jsonContent.career_list.map((career) => {
+    const detail = career.detail.replace(/\\n/g, '\n');
     return (
       <li key={career.title} className={styles.item}>
         <p className={styles.ymd}>{career.ymd}</p>
         <section>
           <h3 className={styles.title}>{career.title}</h3>
-          <p>{career.detail}</p>
+          <p className={styles.detail}>{detail}</p>
         </section>
       </li>
     );
