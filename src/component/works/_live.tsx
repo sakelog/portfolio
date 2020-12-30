15852d6f1243f0853c17df322f3be8f04f128323
sake <sakelog.website@gmail.com>
2020-12-31 08:21:51 +0900

    動作改善

--- src/component/works/_live.tsx
+++ src/component/works/_live.tsx
@@ -7,15 +7,17 @@ const Live: works.component_workslive.func = (props) => {
   const workslive_list = workslive_content.workslive_list.map((item) => {
     return (
       <li key={item.url}>
-        <a href={item.url}>{item.title}</a>
-        <Image
-          src={item.screenshot}
-          alt={item.title + '_スクリーンショット'}
-          width={640}
-          height={480}
-          layout="intrinsic"
-          loading="lazy"
-        />
+        <a href={item.url}>
+          {item.title}
+          <Image
+            src={item.screenshot}
+            alt={item.title + '_スクリーンショット'}
+            width={640}
+            height={480}
+            layout="intrinsic"
+            loading="lazy"
+          />
+        </a>
         <span className={styles.description}>{item.description}</span>
       </li>
     );
