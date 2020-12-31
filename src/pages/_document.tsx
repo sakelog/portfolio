391c040006670e540af0b0b3018cbb5e407fa6c3
sake <sakelog.website@gmail.com>
2020-12-31 11:10:58 +0900

    bug-fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,9 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 const GTM_ID = process.env.GTM_ID;
+const GTMNoScript = `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
+
 class MyDocument extends Document {
   render() {
     return (
@@ -10,15 +13,7 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
           {/*--- Google Tag Manager */}
-          <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-              height="0"
-              width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-            ></iframe>
-          </noscript>
-          {/*--- Google Tag Manager */}
+          <noscript dangerouslySetInnerHTML={{ __html: GTMNoScript }} />
         </body>
       </Html>
     );
