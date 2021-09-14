bd23e6e25a7843a3b3efb1189879a3a193fc7887
sake <sakelog.website@gmail.com>
2021-09-15 00:50:00 +0900

    型定義

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
-import '../styles/global.scss';
+import type { AppProps } from 'next/app';
 
-export default function MyApp({ Component, pageProps }) {
+import '@styles/global.scss';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
