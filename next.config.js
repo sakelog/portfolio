30c97d5d62acd5adee6a027f057e7885688cb88d
sake <sakelog.website@gmail.com>
2021-09-15 22:09:44 +0900

    不要記載削除、image設定

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
-const path = require('path');
 const withPWA = require('next-pwa');
 
 module.exports = withPWA({
   pwa: {
     dest: 'public',
   },
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+  images: {
+    domains: ['media.graphcms.com'],
   },
 });
