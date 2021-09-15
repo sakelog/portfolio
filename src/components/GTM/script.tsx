27c88235a4124500190410890b2f62e6e54a0e04
sake <sakelog.website@gmail.com>
2021-09-15 22:54:30 +0900

    GTM

--- src/components/GTM/script.tsx
+++ src/components/GTM/script.tsx
@@ -4,16 +4,13 @@ const GTMScript = () => {
   const GTM_ID = process.env.GTM_ID;
   return (
     <Script
-      dangerouslySetInnerHTML={{
-        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${GTM_ID}');`,
-      }}
       strategy="afterInteractive"
       id="GTM_Script"
-    />
+    >{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','${GTM_ID}');`}</Script>
   );
 };
 
