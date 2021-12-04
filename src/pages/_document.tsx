15a6912a7fe8ba87c46d66ac15b998288aabf107
sake <sakelog.website@gmail.com>
2021-12-04 11:42:53 +0900

    コンパイルエラー解消

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,50 +1,45 @@
-import Document, {
+import {
   Html,
   Head,
   Main,
   NextScript,
 } from 'next/document';
-
 import GTMNoScript from '@components/GTM/noScript';
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="ja">
-        <Head>
-          <meta charSet="utf-8" />
-          <link
-            rel="preconnect"
-            href="https://fonts.googleapis.com"
-          />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="anonymous"
-          />
-          <link
-            href={
-              'https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Roboto:wght@400;500&display=swap'
-            }
-            rel="stylesheet"
-          />
-          <link
-            href={
-              'https://fonts.googleapis.com/css2?family=Comfortaa&display=swap'
-            }
-            rel="stylesheet"
-          />
-        </Head>
-        <body>
-          {process.env.NODE_ENV === 'production' && (
-            <GTMNoScript />
-          )}
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function MyDocument() {
+  return (
+    <Html lang="ja">
+      <Head>
+        <meta charSet="utf-8" />
+        <link
+          rel="preconnect"
+          href="https://fonts.googleapis.com"
+        />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href={
+            'https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Roboto:wght@400;500&display=swap'
+          }
+          rel="stylesheet"
+        />
+        <link
+          href={
+            'https://fonts.googleapis.com/css2?family=Comfortaa&display=swap'
+          }
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        {process.env.NODE_ENV === 'production' && (
+          <GTMNoScript />
+        )}
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument;
