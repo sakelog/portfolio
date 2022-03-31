0b622d766833ceac9d57b73126f3d3fad79de23f
sake <sakelog.website@gmail.com>
2022-03-31 23:13:32 +0900

    レイアウト修正

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -9,7 +9,7 @@ const Layout = (props: PropsType) => {
   return (
     <>
       <HeroHeader />
-      <main className="container mx-auto px-2 py-4">
+      <main className="container mx-auto px-4 xl:px-6 py-4">
         {props.children}
       </main>
       <Footer />
