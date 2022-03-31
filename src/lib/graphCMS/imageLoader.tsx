7ce449f6e78dee2518b9b2d581945659781d0e07
sake <sakelog.website@gmail.com>
2022-03-31 23:35:52 +0900

    assetドメイン修正

--- src/lib/graphCMS/imageLoader.tsx
+++ src/lib/graphCMS/imageLoader.tsx
@@ -7,7 +7,7 @@ export const imageLoader = (props: PropsType) => {
   const { src, width } = props;
   const relativeSrc = (src: string) => src.split('/').pop();
 
-  return `https://media.graphcms.com/resize=width:${width}/output=format:webp/${relativeSrc(
+  return `https://media.graphassets.com/resize=width:${width}/output=format:webp/${relativeSrc(
     src
   )}`;
 };
