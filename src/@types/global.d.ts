cc063eab56f5cb5414b019bedfbf72532b377c78
sake <sakelog.website@gmail.com>
2022-03-31 23:13:46 +0900

    formコンポーネント分割

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -1,1 +1,28 @@
+declare namespace Form {
+  declare type FormLabel = {
+    icon: ReactElement;
+    label: string;
+  }
+  declare type FormInput = {
+    type: 'text' | 'email' | 'textarea';
+    placeholder?: string;
+  }
 
+  declare type Base = {
+    id: Id;
+    required?: boolean;
+  }
+  declare namespace Item  {
+    declare type Props = 
+    Base &
+     {
+      state: {
+        submitting: boolean;
+        succeeded: boolean;
+        errors: any[];
+      };
+      formLabel: FormLabel;
+      formInput: FormInput;
+    }
+  }
+}
--- src/components/contactForm.tsx
+++ src/components/contactForm.tsx
@@ -6,6 +6,75 @@ import {
   HiOutlineChat,
 } from 'react-icons/hi';
 
+const FormItem = (props: Form.Item.Props) => {
+  return (
+    <div className="grid grid-cols-1 gap-1 md:grid-cols-3 items-baseline">
+      <FormLabel
+        id={props.id}
+        icon={props.formLabel.icon}
+        label={props.formLabel.label}
+        required={props.required}
+      />
+      <FormInput
+        id={props.id}
+        type={props.formInput.type}
+        placeholder={props.formInput.placeholder}
+        required={props.required}
+      />
+      <ValidationError
+        prefix={props.id}
+        field={props.id}
+        errors={props.state.errors}
+      />
+    </div>
+  );
+};
+
+type LabelPropsType = Form.Base & Form.FormLabel;
+const FormLabel = (props: LabelPropsType) => {
+  return (
+    <label
+      className="grid grid-cols-9 gap-1 items-center"
+      htmlFor={props.id}
+    >
+      <span className="col-span-full md:col-span-2">
+        {props.required && (
+          <span className="flex md:justify-center bg-red-100 border-2 border-red-500">
+            <span className=" text-xs p-1">必須</span>
+          </span>
+        )}
+      </span>
+      {props.icon}
+      <span className="col-span-8 md:col-span-6">
+        {props.label}
+      </span>
+    </label>
+  );
+};
+
+type InputPropsType = Form.Base & Form.FormInput;
+const FormInput = (props: InputPropsType) => {
+  return props.type == 'textarea' ? (
+    <textarea
+      name={props.id}
+      id={props.id}
+      rows={5}
+      placeholder={props.placeholder}
+      className="rounded md:col-span-2"
+      required={props.required}
+    />
+  ) : (
+    <input
+      name={props.id}
+      id={props.id}
+      type={props.type}
+      placeholder={props.placeholder}
+      className="rounded md:col-span-2"
+      required={props.required}
+    />
+  );
+};
+
 const ContactForm = () => {
   const [state, handleSubmit] = useForm(
     process.env.NEXT_PUBLIC_FORMSPREE_ENDPOINT || ''
@@ -21,79 +90,46 @@ const ContactForm = () => {
   return (
     <section>
       <form
-        className="space-y-3 max-w-screen-sm mx-auto"
+        className="space-y-6 bg-gray-100 rounded p-2 max-w-screen-sm mx-auto"
         onSubmit={handleSubmit}
       >
-        <div className="grid grid-cols-1 gap-2 md:grid-cols-3 items-baseline">
-          <label
-            className="flex items-center space-x-1 py-2 md:py-0"
-            htmlFor="name"
-          >
-            <HiOutlineUser />
-            名前
-          </label>
-          <input
-            name="name"
-            id="name"
-            type="text"
-            placeholder="サンプル　花子"
-            className="rounded md:col-span-2"
-          />
-          <ValidationError
-            prefix="Name"
-            field="name"
-            errors={state.errors}
-          />
-        </div>
-        <div className="grid grid-cols-1 gap-2 md:grid-cols-3 items-baseline">
-          <label
-            className="flex items-center space-x-1 py-2 md:py-0"
-            htmlFor="email"
-          >
-            <HiOutlineMail />
-            メールアドレス
-            <span className="bg-red-500 text-white text-sm p-1">
-              必須
-            </span>
-          </label>
-          <input
-            name="email"
-            id="email"
-            type="email"
-            placeholder="sample@email.com"
-            className="rounded md:col-span-2"
-            required
-          />
-          <ValidationError
-            prefix="Email"
-            field="email"
-            errors={state.errors}
-          />
-        </div>
-        <div className="grid grid-cols-1 gap-2 md:grid-cols-3 items-baseline">
-          <label
-            className="flex items-center space-x-1 py-2 md:py-0"
-            htmlFor="comment"
-          >
-            <HiOutlineChat />
-            お問い合わせ内容
-            <span className="bg-red-500 text-white text-sm p-1">
-              必須
-            </span>
-          </label>
-          <textarea
-            name="comment"
-            id="comment"
-            className="rounded md:col-span-2"
-            rows={5}
-            required
-          />
-          <ValidationError
-            prefix="Comment"
-            field="comment"
-            errors={state.errors}
-          />
-        </div>
+        <FormItem
+          id="name"
+          formLabel={{
+            icon: <HiOutlineUser />,
+            label: '名前',
+          }}
+          formInput={{
+            type: 'text',
+            placeholder: 'サンプル　花子',
+          }}
+          state={state}
+        ></FormItem>
+        <FormItem
+          id="email"
+          formLabel={{
+            icon: <HiOutlineMail />,
+            label: 'メールアドレス',
+          }}
+          formInput={{
+            type: 'email',
+            placeholder: 'sample@email.com',
+          }}
+          required
+          state={state}
+        ></FormItem>
+        <FormItem
+          id="comment"
+          formLabel={{
+            icon: <HiOutlineChat />,
+            label: 'お問い合わせ内容',
+          }}
+          formInput={{
+            type: 'textarea',
+          }}
+          required
+          state={state}
+        ></FormItem>
         <button
           type="submit"
           className="bg-theme text-white px-4 py-2 block w-32 mx-auto rounded"
