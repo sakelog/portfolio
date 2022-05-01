4bdc896911b08bc8216278c0a364e0df74e1d5f9
sake <sakelog.website@gmail.com>
2022-05-01 14:46:18 +0900

    ogp path修正

--- src/pages/ogp.tsx
+++ src/pages/ogp.tsx
@@ -2,17 +2,19 @@ import { useRouter } from 'next/router';
 import type { NextPage } from 'next';
 import Image from 'next/image';
 
+// components
+import siteMeta from 'components/config';
+
 const OgpPage: NextPage = () => {
   const router = useRouter();
   const { title } = router.query;
 
   return (
     <div
-      className="bg-theme-dark text-white 
-  grid grid-cols-3"
-      style={{ width: 1200, height: 600 }}
+      style={{ width: 1200, height: 630 }}
+      className="grid grid-cols-3"
     >
-      <div className="flex items-center justify-center">
+      <div className="bg-theme flex items-center justify-center">
         <Image
           src="/img/profile.png"
           alt="profile icon"
@@ -22,8 +24,9 @@ const OgpPage: NextPage = () => {
           className="rounded-full bg-white"
         />
       </div>
-      <div className="col-span-2 flex items-center justify-center">
+      <div className="col-span-2 flex flex-col gap-2 items-center justify-center">
         <p className="text-6xl">{title}</p>
+        <p className="text-2xl">{siteMeta.url}</p>
       </div>
     </div>
   );
