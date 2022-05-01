4a80fefc204ad171085891bb05b577da5e379fae
sake <sakelog.website@gmail.com>
2022-05-01 09:03:58 +0900

    Skill修正

---
+++ src/components/Skill/Legend.tsx
@@ -0,0 +1,70 @@
+import StarIconCollection from 'components/Skill/StarIconCollection';
+
+const LegendItem = ({
+  times,
+  detail,
+}: {
+  times: number;
+  detail: React.ReactNode;
+}) => (
+  <div>
+    <StarIconCollection
+      keyphrase={`fixed${times}`}
+      times={times}
+    />
+    <div className="text-gray-600 text-sm px-4">
+      {detail}
+    </div>
+  </div>
+);
+
+const Legend = () => (
+  <section className="space-y-2 bg-gray-light p-4 rounded-md overflow-hidden">
+    <h4 className="mb-4 py-2 border-b border-gray text-center">
+      基準
+    </h4>
+    <LegendItem
+      times={5}
+      detail={
+        <>
+          <p>業務で10案件以上経験あり</p>
+          <p>個人開発で10年以上経験あり</p>
+        </>
+      }
+    />
+    <LegendItem
+      times={4}
+      detail={
+        <>
+          <p>業務で5案件以上経験あり</p>
+          <p>個人開発で5年以上経験あり</p>
+        </>
+      }
+    />
+    <LegendItem
+      times={3}
+      detail={
+        <>
+          <p>業務で1案件以上経験あり</p>
+          <p>個人開発で5案件以上経験あり</p>
+          <p>関連資格取得済み</p>
+        </>
+      }
+    />
+    <LegendItem
+      times={2}
+      detail={<p>個人開発で3案件以上経験あり</p>}
+    />
+    <LegendItem
+      times={1}
+      detail={
+        <>
+          <p>個人開発で1案件以上経験あり</p>
+          <p>関連資格の学習中</p>
+        </>
+      }
+    />
+  </section>
+);
+
+export default Legend;
--- src/components/Skill/index.tsx
+++ src/components/Skill/index.tsx
@@ -1,74 +1,57 @@
-import { HiStar as StarIcon } from 'react-icons/hi';
+import StarIconCollection from 'components/Skill/StarIconCollection';
+import Legend from 'components/Skill/Legend';
 
-type PropTypes = {
+const SkillLists = ({
+  skillLists,
+}: {
   skillLists: Skill.Collections;
-};
-
-const SkillListsComponent = ({ skillLists }: PropTypes) => (
-  <div>
-    {skillLists?.map((skillList) => (
+}) => (
+  <>
+    {skillLists.map((skillList) => (
       <section
         key={skillList.id}
-        className="overflow-hidden border-2 border-theme rounded p-4"
+        className="border-2 border-gray overflow-hidden p-4 rounded-md"
       >
-        <h4 className="mb-4">{skillList.category}</h4>
+        <h3 className="mb-4 py-2 border-b border-gray text-center">
+          {skillList.category}
+        </h3>
         <ul>
-          {skillList.skills.map((skill) => {
-            const MAX_STAR = 5;
-            const yellowStar = Array(skill.star);
-            for (
-              let index = 0;
-              index < yellowStar.length;
-              index += 1
-            ) {
-              yellowStar[index] = (
-                <StarIcon
-                  className="text-yellow-400"
-                  key={index}
-                />
-              );
-            }
-            const grayStar = Array(MAX_STAR - skill.star);
-            for (
-              let index = 0;
-              index < grayStar.length;
-              index += 1
-            ) {
-              grayStar[index] = (
-                <StarIcon
-                  className="text-gray-400"
-                  key={index}
-                />
-              );
-            }
-            return (
-              <li
-                key={skill.id}
-                className="grid grid-cols-3 gap-2 items-center py-2"
-              >
-                <span className="font-bold">
-                  {skill.name}
-                </span>
-                <span className="col-span-2 flex">
-                  {yellowStar}
-                  {grayStar}
-                </span>
-              </li>
-            );
-          })}
+          {skillList.skills.map((skill) => (
+            <li
+              key={skill.id}
+              className="grid grid-cols-1 sm:grid-cols-3 gap-2 
+                items-center py-2"
+            >
+              <span className="font-bold">
+                {skill.name}
+              </span>
+              <StarIconCollection
+                keyphrase={skill.name}
+                times={skill.star}
+              />
+            </li>
+          ))}
         </ul>
       </section>
-    )) || null}
-  </div>
+    ))}
+  </>
 );
 
-const Skill = ({ skillLists }: PropTypes) => (
-  <section>
-    <h3 className="u__header--subTitle">スキル</h3>
-    <section className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-      <SkillListsComponent skillLists={skillLists} />
+const Skill = ({
+  skillLists,
+}: {
+  skillLists: Skill.Collections;
+}) => (
+  <section className="overflow-hidden">
+    <h2 className="c-heading__main-section" id="skill">
+      スキル
+    </h2>
+    <section className="my-4 grid grid-cols-1 gap-y-4 overflow-hidden">
+      <Legend />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-y-4 md:gap-x-4">
+        <SkillLists skillLists={skillLists} />
+      </div>
     </section>
   </section>
 );
-
 export default Skill;
