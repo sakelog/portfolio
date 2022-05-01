9f0684da65b2c6f3067505e4ea8f9722e8ccff04
sake <sakelog.website@gmail.com>
2022-05-01 09:43:24 +0900

    Head分割

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage, GetStaticProps } from 'next';
-import Head from 'next/head';
 
 // lib
 import { setSiteMap } from 'lib/setStitemap';
@@ -9,12 +8,12 @@ import {
   skill,
   qualification,
 } from 'lib/graphCMS/getContent';
-import { repo } from 'lib/github/getContent';
+import github from 'lib/github/getContent';
+import qiita from 'lib/qiita/getContent';
 
 // components
-import siteMeta from 'components/config';
-
 import Layout from 'layout';
+import MyHead from 'components/MyHead';
 import Profile from 'components/Profile';
 import Skill from 'components/Skill';
 import Works from 'components/Works';
@@ -22,294 +21,22 @@ import ContactForm from 'components/ContactForm';
 
 type PageProps = {
   profileItem: {
-    aboutMeItems: Profile.AboutMe.Items;
-    carreerItems: Profile.Carreer.Items;
-    qualifications: Profile.Qualification.Items;
+    aboutMeItems?: Profile.AboutMe.Items;
+    carreerItems?: Profile.Carreer.Items;
+    qualifications?: Profile.Qualification.Items;
   };
-  skillItem: {
-    skillLists: Skill.Collections;
-  };
+  skillLists?: Skill.Collections;
   worksItem: {
-    githubRepos: Works.GitHub.Repositories;
+    githubRepos?: Works.GitHub.Repositories;
+    qiitaArticles?: Works.Qiita.Article[];
   };
 };
 
-const MyHead = () => (
-  <Head>
-    <title>{siteMeta.title}</title>
-    <meta
-      name="description"
-      content={siteMeta.description}
-    />
-    <meta property="og:title" content={siteMeta.title} />
-    <meta
-      property="og:description"
-      content={siteMeta.description}
-    />
-    <meta property="og:type" content="website" />
-    <meta
-      property="og:image"
-      content={`${siteMeta.url}/img/ogimage.png`}
-    />
-    <meta
-      name="twitter:card"
-      content="summary_large_image"
-    />
-    <meta
-      name="twitter:description"
-      content={siteMeta.description}
-    />
-    <meta name="theme-color" content="#3675a9" />
-    <meta
-      name="msapplication-square70x70logo"
-      content="/img/icon/site-tile-70x70.png"
-    />
-    <meta
-      name="msapplication-square150x150logo"
-      content="/img/icon/site-tile-150x150.png"
-    />
-    <meta
-      name="msapplication-wide310x150logo"
-      content="/img/icon/site-tile-310x150.png"
-    />
-    <meta
-      name="msapplication-square310x310logo"
-      content="/site-tile-310x310.png"
-    />
-    <meta
-      name="msapplication-TileColor"
-      content="#0078d7"
-    />
-    <link
-      rel="shortcut icon"
-      type="image/vnd.microsoft.icon"
-      href="/favicon.ico"
-    />
-    <link
-      rel="icon"
-      type="image/vnd.microsoft.icon"
-      href="/favicon.ico"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="57x57"
-      href="/img/icon/apple-touch-icon-57x57.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="60x60"
-      href="/img/icon/apple-touch-icon-60x60.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="72x72"
-      href="/img/icon/apple-touch-icon-72x72.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="76x76"
-      href="/img/icon/apple-touch-icon-76x76.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="114x114"
-      href="/img/icon/apple-touch-icon-114x114.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="120x120"
-      href="/img/icon/apple-touch-icon-120x120.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="144x144"
-      href="/img/icon/apple-touch-icon-144x144.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="152x152"
-      href="/img/icon/apple-touch-icon-152x152.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="180x180"
-      href="/img/icon/apple-touch-icon-180x180.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="36x36"
-      href="/img/icon/android-chrome-36x36.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="48x48"
-      href="/img/icon/android-chrome-48x48.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="72x72"
-      href="/img/icon/android-chrome-72x72.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="96x96"
-      href="/img/icon/android-chrome-96x96.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="128x128"
-      href="/img/icon/android-chrome-128x128.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="144x144"
-      href="/img/icon/android-chrome-144x144.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="152x152"
-      href="/img/icon/android-chrome-152x152.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="192x192"
-      href="/img/icon/android-chrome-192x192.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="256x256"
-      href="/img/icon/android-chrome-256x256.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="384x384"
-      href="/img/icon/android-chrome-384x384.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="512x512"
-      href="/img/icon/android-chrome-512x512.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="36x36"
-      href="/img/icon/icon-36x36.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="48x48"
-      href="/img/icon/icon-48x48.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="72x72"
-      href="/img/icon/icon-72x72.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="96x96"
-      href="/img/icon/icon-96x96.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="128x128"
-      href="/img/icon/icon-128x128.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="144x144"
-      href="/img/icon/icon-144x144.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="152x152"
-      href="/img/icon/icon-152x152.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="160x160"
-      href="/img/icon/icon-160x160.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="192x192"
-      href="/img/icon/icon-192x192.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="196x196"
-      href="/img/icon/icon-196x196.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="256x256"
-      href="/img/icon/icon-256x256.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="384x384"
-      href="/img/icon/icon-384x384.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="512x512"
-      href="/img/icon/icon-512x512.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="16x16"
-      href="/img/icon/icon-16x16.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="24x24"
-      href="/img/icon/icon-24x24.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="32x32"
-      href="/img/icon/icon-32x32.png"
-    />
-    <link rel="manifest" href="/manifest.json" />
-  </Head>
-);
-
-const TopPage: NextPage<PageProps> = ({
-  profileItem,
-  skillItem,
-  worksItem,
-}: PageProps) => {
+const TopPage: NextPage<PageProps> = (props) => {
+  const { profileItem, skillLists, worksItem } = props;
   const { aboutMeItems, carreerItems, qualifications } =
     profileItem;
-  const { skillLists } = skillItem;
-  const { githubRepos } = worksItem;
+  const { githubRepos, qiitaArticles } = worksItem;
   return (
     <Layout>
       <MyHead />
@@ -319,31 +46,11 @@ const TopPage: NextPage<PageProps> = ({
         qualifications={qualifications}
       />
       <Skill skillLists={skillLists} />
-      <Works githubRepos={githubRepos} />
+      <Works
+        githubRepos={githubRepos}
+        qiitaArticles={qiitaArticles}
+      />
       <ContactForm />
-      {/* <h2 className="u__header--mainTitle" id="profile">
-        自己紹介
-      </h2>
-      <AboutMeComponent
-        aboutMeItems={profileItem.aboutMeItems}
-      />
-      <CarrerComponent
-        carreerItems={profileItem.carreerItems}
-      />
-      <SkillComponent skillLists={profileItem.skillLists} />
-      <QualificationComponent
-        qualifications={profileItem.qualifications}
-      />
-      <h2 className="u__header--mainTitle" id="works">
-        作成したもの
-      </h2>
-      <GithubRepoComponent
-        githubRepos={worksItem.githubRepos}
-      />
-      <h2 className="u__header--mainTitle" id="contact">
-        お問い合わせ
-      </h2>
-      <ContactForm /> */}
     </Layout>
   );
 };
@@ -359,11 +66,13 @@ export const getStaticProps: GetStaticProps<
   const qualifications =
     await qualification.getQualifications();
 
-  const githubRepos = await repo.getRepositories();
+  const githubRepos = await github.repo.getRepositories();
+  const qiitaArticles = await qiita.article.getArticles();
 
   const fetchDate = new Date();
-
-  setSiteMap(fetchDate);
+  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+  process.env.NODE_ENV === 'production' &&
+    setSiteMap(fetchDate);
 
   return {
     props: {
@@ -372,11 +81,10 @@ export const getStaticProps: GetStaticProps<
         carreerItems,
         qualifications,
       },
-      skillItem: {
-        skillLists,
-      },
+      skillLists,
       worksItem: {
         githubRepos,
+        qiitaArticles,
       },
     },
   };
