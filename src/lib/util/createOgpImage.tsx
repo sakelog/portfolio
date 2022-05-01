040d2e2c351e69ed95dadff54f19667873585d71
sake <sakelog.website@gmail.com>
2022-05-01 15:18:16 +0900

    ogpimage自動生成

---
+++ src/lib/util/createOgpImage.tsx
@@ -0,0 +1,62 @@
+import { join } from 'path';
+import fs from 'fs';
+import { chromium } from 'playwright';
+
+// config
+import siteMeta from 'components/config';
+
+export const createOgpImage = async (title: string) => {
+  if (process.env.NODE_ENV === 'production') {
+    const ogpPagePath = `${
+      siteMeta.url
+    }/ogp?title=${decodeURIComponent(title)}`;
+
+    const localOgpImageDir = join(
+      process.cwd(),
+      'public',
+      'img',
+      'ogp'
+    );
+    const ogpImageFileName = `${decodeURIComponent(
+      title
+    )}.png`;
+
+    const localPath = join(
+      localOgpImageDir,
+      ogpImageFileName
+    );
+    const publicPath = `${siteMeta.url}/public/img/ogp/${ogpImageFileName}`;
+
+    console.log(localPath);
+
+    try {
+      fs.statSync(localPath);
+      return publicPath;
+    } catch (e) {
+      console.log(`ogp image create for : ${title}`);
+    }
+
+    const browser = await chromium.launch({
+      headless: true,
+    });
+    const page = await browser.newPage();
+    await page.setViewportSize({
+      width: 1200,
+      height: 630,
+    });
+    await page.goto(ogpPagePath, {
+      waitUntil: 'networkidle',
+    });
+    const buffer = await page.screenshot({ type: 'png' });
+    await browser.close();
+
+    fs.mkdirSync(localOgpImageDir, { recursive: true });
+    fs.writeFileSync(localPath, buffer);
+
+    return publicPath;
+  }
+
+  return '';
+};
+
+export default createOgpImage;
