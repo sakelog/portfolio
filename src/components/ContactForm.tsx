419908433ffcb670d7dc6e68e643f198709379b3
sake <sakelog.website@gmail.com>
2022-05-01 09:25:43 +0900

    ContactFormデザイン修正

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,43 +1,41 @@
+// lib
 import { useForm, ValidationError } from '@formspree/react';
 
+// components
 import {
   HiOutlineUser,
   HiOutlineMail,
   HiOutlineChat,
 } from 'react-icons/hi';
 
-type LabelPropTypes = Form.Base & Form.FormLabel;
+type LabelPropsType = Form.Base & Form.FormLabel;
 const FormLabel = ({
   id,
-  required,
   icon,
   label,
-}: LabelPropTypes) => (
+  required,
+}: LabelPropsType) => (
   <label
-    className="grid grid-cols-9 gap-1 items-center"
+    className="space-y-2 my-2 md:flex md:justify-between md:items-center"
     htmlFor={id}
   >
-    <span className="col-span-full md:col-span-2">
-      {required && (
-        <span className="flex md:justify-center bg-red-100 border-2 border-red-500">
-          <span className=" text-xs p-1">必須</span>
-        </span>
-      )}
-    </span>
-    {icon}
-    <span className="col-span-8 md:col-span-6">
-      {label}
-    </span>
+    <div className="flex items-center gap-2">
+      {icon}
+      <span className="">{label}</span>
+    </div>
+    {required && (
+      <span className="c-badge c-badge--red">必須</span>
+    )}
   </label>
 );
 
-type InputPropTypes = Form.Base & Form.FormInput;
+type InputPropsType = Form.Base & Form.FormInput;
 const FormInput = ({
   type,
   id,
   placeholder,
   required,
-}: InputPropTypes) =>
+}: InputPropsType) =>
   type === 'textarea' ? (
     <textarea
       name={id}
@@ -60,12 +58,12 @@ const FormInput = ({
 
 const FormItem = ({
   id,
-  required,
   formLabel,
+  required,
   formInput,
   state,
 }: Form.Item.Props) => (
-  <div className="grid grid-cols-1 gap-1 md:grid-cols-3 items-baseline">
+  <div className="grid grid-cols-1">
     <FormLabel
       id={id}
       icon={formLabel.icon}
@@ -97,55 +95,60 @@ const ContactForm = () => {
     </div>
   ) : (
     <section>
-      <form
-        className="space-y-6 bg-gray-100 rounded p-2 max-w-screen-sm mx-auto"
-        onSubmit={handleSubmit}
-      >
-        <FormItem
-          id="name"
-          formLabel={{
-            icon: <HiOutlineUser />,
-            label: '名前',
-          }}
-          formInput={{
-            type: 'text',
-            placeholder: 'サンプル　花子',
-          }}
-          state={state}
-        />
-        <FormItem
-          id="email"
-          formLabel={{
-            icon: <HiOutlineMail />,
-            label: 'メールアドレス',
-          }}
-          formInput={{
-            type: 'email',
-            placeholder: 'sample@email.com',
-          }}
-          required
-          state={state}
-        />
-        <FormItem
-          id="comment"
-          formLabel={{
-            icon: <HiOutlineChat />,
-            label: 'お問い合わせ内容',
-          }}
-          formInput={{
-            type: 'textarea',
-          }}
-          required
-          state={state}
-        />
-        <button
-          type="submit"
-          className="bg-theme text-white px-4 py-2 block w-32 mx-auto rounded"
-          disabled={state.submitting}
+      <h2 className="c-heading__main-section" id="contact">
+        お問い合わせ
+      </h2>
+      <div className="bg-gray-light my-4">
+        <form
+          className="p-4 space-y-4 md:max-w-screen-sm md:mx-auto"
+          onSubmit={handleSubmit}
         >
-          送信
-        </button>
-      </form>
+          <FormItem
+            id="name"
+            formLabel={{
+              icon: <HiOutlineUser size="20" />,
+              label: '名前',
+            }}
+            formInput={{
+              type: 'text',
+              placeholder: 'サンプル　花子',
+            }}
+            state={state}
+          />
+          <FormItem
+            id="email"
+            formLabel={{
+              icon: <HiOutlineMail size="20" />,
+              label: 'メールアドレス',
+            }}
+            formInput={{
+              type: 'email',
+              placeholder: 'sample@email.com',
+            }}
+            required
+            state={state}
+          />
+          <FormItem
+            id="comment"
+            formLabel={{
+              icon: <HiOutlineChat size="20" />,
+              label: 'お問い合わせ内容',
+            }}
+            formInput={{
+              type: 'textarea',
+            }}
+            required
+            state={state}
+          />
+          <button
+            type="submit"
+            className="bg-theme text-white px-4 py-2 block w-32 mx-auto rounded"
+            disabled={state.submitting}
+          >
+            送信
+          </button>
+        </form>
+      </div>
     </section>
   );
 };
